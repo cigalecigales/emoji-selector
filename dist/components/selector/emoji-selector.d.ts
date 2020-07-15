fd38782a9b2b7476e4f61a990dfa56615b1128fe
cigalecigales <cigalecigales@gmail.com>
2020-07-15 23:12:32 +0900

    build

--- dist/components/selector/emoji-selector.d.ts
+++ dist/components/selector/emoji-selector.d.ts
@@ -5,12 +5,14 @@ interface EmojiSelectorProps {
 }
 interface EmojiSelectorState {
     fileter: string;
+    emojiList: JSX.Element[];
 }
 /**
  * EmojiSelector component
  */
 declare class EmojiSelector extends React.Component<EmojiSelectorProps, EmojiSelectorState> {
     constructor(props: EmojiSelectorProps);
+    onChange(e: React.ChangeEvent<HTMLDivElement>): void;
     render(): JSX.Element;
 }
 export default EmojiSelector;
--- dist/components/selector/emoji-selector.js
+++ dist/components/selector/emoji-selector.js
@@ -26,26 +26,34 @@ var EmojiSelector = /** @class */ (function (_super) {
     __extends(EmojiSelector, _super);
     function EmojiSelector(props) {
         var _this = _super.call(this, props) || this;
-        _this.state = {
-            fileter: ''
-        };
-        return _this;
-    }
-    EmojiSelector.prototype.render = function () {
-        var _this = this;
         var emojiList = [];
         Object.keys(emojis).forEach(function (key) {
             emojiList.push(React.createElement(Emoji, { name: key, key: key, onAddEmoji: _this.props.onAddEmoji }));
         });
+        _this.state = {
+            fileter: '',
+            emojiList: emojiList
+        };
+        return _this;
+    }
+    EmojiSelector.prototype.onChange = function (e) {
+        var value = e.target.value;
+        this.setState({
+            fileter: value
+        });
+        console.log(value);
+    };
+    EmojiSelector.prototype.render = function () {
         return (React.createElement(EmojiSelectorStyle, { show: this.props.show },
-            React.createElement("div", { className: "emojiList" }, emojiList)));
+            React.createElement("input", { type: 'text', onChange: this.onChange }),
+            React.createElement("div", { className: "emojiList" }, this.state.emojiList)));
     };
     return EmojiSelector;
 }(React.Component));
 /**
  * EmojiSelector styles
  */
-var EmojiSelectorStyle = styled.div(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n  font-size: 22px;\n  box-sizing: border-box;\n  width: 360px;\n  height: 200px;\n  overflow-y: scroll;\n  border: 1px solid rgba(29, 28, 29, 0.2);\n  word-wrap: break-word;\n  position: absolute;\n  top: 30px;\n  background: #ffffff;\n  ", ";\n\n  .emojiList {\n    text-align: center;\n  }\n"], ["\n  font-size: 22px;\n  box-sizing: border-box;\n  width: 360px;\n  height: 200px;\n  overflow-y: scroll;\n  border: 1px solid rgba(29, 28, 29, 0.2);\n  word-wrap: break-word;\n  position: absolute;\n  top: 30px;\n  background: #ffffff;\n  ",
+var EmojiSelectorStyle = styled.div(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n  font-size: 22px;\n  box-sizing: border-box;\n  width: 360px;\n  height: 200px;\n  overflow-y: scroll;\n  border: 1px solid rgba(29, 28, 29, 0.2);\n  word-wrap: break-word;\n  position: absolute;\n  top: 30px;\n  background: #ffffff;\n  z-index: 1000;\n  ", ";\n\n  .emojiList {\n    text-align: center;\n  }\n"], ["\n  font-size: 22px;\n  box-sizing: border-box;\n  width: 360px;\n  height: 200px;\n  overflow-y: scroll;\n  border: 1px solid rgba(29, 28, 29, 0.2);\n  word-wrap: break-word;\n  position: absolute;\n  top: 30px;\n  background: #ffffff;\n  z-index: 1000;\n  ",
     ";\n\n  .emojiList {\n    text-align: center;\n  }\n"])), function (_a) {
     var show = _a.show;
     return show ? '' : 'display: none;';
